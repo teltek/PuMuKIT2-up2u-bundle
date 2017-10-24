e957c4a85596c2cf087c66b6fb31ff215029c54e
rubenrua <rubenrua@teltek.es>
2017-10-24 13:17:16 +0200

    Move IframeController::iframeAction to MultimediaObjectController::iframeAction

--- Controller/IframeController.php
+++ Controller/IframeController.php
@@ -3,12 +3,8 @@
 namespace Pumukit\Geant\WebTVBundle\Controller;
 
 use Pumukit\WebTVBundle\Controller\PlayerController;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Pumukit\SchemaBundle\Document\MultimediaObject;
 
 class IframeController extends PlayerController
@@ -16,23 +12,14 @@ class IframeController extends PlayerController
     /**
      * @Template("PumukitWebTVBundle:MultimediaObject:iframeplayer.html.twig")
      */
-    public function indexAction( MultimediaObject $multimediaObject, Request $request ){
+    public function indexAction(MultimediaObject $multimediaObject, Request $request)
+    {
         $iframeUrl = $multimediaObject->getProperty('iframe_url');
         $this->updateBreadcrumbs($multimediaObject);
-        return array('multimediaObject' => $multimediaObject,
-                     'iframe_url' => $iframeUrl);
-    }
 
-    /**
-     * @Route("/iframe/{id}", name="pumukit_webtv_multimediaobject_iframe" )
-     * @Template()
-     */
-    public function iframeAction(MultimediaObject $multimediaObject, Request $request)
-    {
-        if ($multimediaObject->getProperty('iframeable') === true ) {
-            $iframeUrl = $multimediaObject->getProperty('iframe_url');
-            return $this->redirect($iframeUrl);
-        }
-        return parent::iframeAction($multimediaObject, $request);
+        return array(
+            'multimediaObject' => $multimediaObject,
+            'iframe_url' => $iframeUrl, 
+        );
     }
 }
--- Controller/MultimediaObjectController.php
+++ Controller/MultimediaObjectController.php
@@ -1,32 +1,51 @@
 <?php
+
 namespace Pumukit\Geant\WebTVBundle\Controller;
 
-use Pumukit\WebTVBundle\Controller\MultimediaObjectController as ParentController;
 use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Pumukit\SchemaBundle\Document\MultimediaObject;
 use Pumukit\SchemaBundle\Document\Track;
 use Pumukit\BasePlayerBundle\Event\BasePlayerEvents;
 use Pumukit\BasePlayerBundle\Event\ViewedEvent;
+use Pumukit\WebTVBundle\Controller\MultimediaObjectController as ParentController;
 
 class MultimediaObjectController extends ParentController
 {
     public function preExecute(MultimediaObject $multimediaObject, Request $request, $secret = false)
     {
-        if ($multimediaObject->getProperty('iframeable') === true ) {
+        if ($multimediaObject->getProperty('iframeable') === true) {
             $this->dispatchViewEvent($multimediaObject);
+
             return $this->forward('PumukitGeantWebTVBundle:Iframe:index', array('request' => $request, 'multimediaObject' => $multimediaObject));
-        }
-        else if ($multimediaObject->getProperty('redirect') === true ) {
+        } elseif ($multimediaObject->getProperty('redirect') === true) {
             $this->dispatchViewEvent($multimediaObject);
             $redirectUrl = $multimediaObject->getProperty('redirect_url');
             if (!$redirectUrl) {
                 throw $this->createNotFoundException();
             }
-            if(strpos($redirectUrl,'://')===false) {
-                $redirectUrl = "http://".$redirectUrl;
+            if (strpos($redirectUrl, '://') === false) {
+                $redirectUrl = 'http://'.$redirectUrl;
             }
+
             return $this->redirect($redirectUrl);
         }
+    }
+
+    /**
+     * @Route("/iframe/{id}", name="pumukit_webtv_multimediaobject_iframe" )
+     * @Template()
+     */
+    public function iframeAction(MultimediaObject $multimediaObject, Request $request)
+    {
+        if ($multimediaObject->getProperty('iframeable') === true) {
+            $iframeUrl = $multimediaObject->getProperty('iframe_url');
+
+            return $this->redirect($iframeUrl);
+        }
+
+        return parent::iframeAction($multimediaObject, $request);
     }
 
     public function dispatchViewEvent(MultimediaObject $multimediaObject, Track $track = null)
