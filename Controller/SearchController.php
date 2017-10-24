ff27f4e6b75bb48489a49e1a773fcb5bea4e47f6
rubenrua <rubenrua@teltek.es>
2017-10-24 14:05:35 +0200

    Add SearchController

---
+++ Controller/SearchController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Pumukit\Geant\WebTVBundle\Controller;
+
+use Pumukit\WebTVBundle\Controller\SearchController as ParentController;
+
+class SearchController extends ParentController
+{
+    protected function createMultimediaObjectQueryBuilder()
+    {
+        $repo = $this->get('doctrine_mongodb')->getRepository('PumukitSchemaBundle:MultimediaObject');
+        $request = $this->get('request_stack')->getMasterRequest();
+
+        if ('/pumoodle/searchmultimediaobjects' == $request->getPathInfo()) {
+            $dm = $this->get('doctrine_mongodb.odm.document_manager');
+            $dm->getFilterCollection()->disable('frontend');
+            $queryBuilder = $repo->createQueryBuilder();
+            $queryBuilder->field('status')->equals(0);
+            $queryBuilder->field('properties.redirect')->equals(false);
+        } else {
+            $queryBuilder = $repo->createStandardQueryBuilder();
+        }
+
+        return $queryBuilder;
+    }
+}
