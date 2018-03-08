22936fd300a32bf4b321645981b73945d9901c39
rubenrua <rubenrua@teltek.es>
2018-03-08 16:42:19 +0100

    Change tooltip text on the text editor toolbar
    https://github.com/up2university/eduoer/issues/3

--- Resources/data/pumoodle/editor/atto/plugins/pumukit/lang/en/atto_pumukit.php
+++ Resources/data/pumoodle/editor/atto/plugins/pumukit/lang/en/atto_pumukit.php
@@ -22,7 +22,7 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$string['pluginname'] = 'PuMuKIT (Atto)';
+$string['pluginname'] = 'Select eduOER content';
 $string['settings'] = 'PuMuKIT (Atto)';
 $string['dialogtitle'] = 'Modal dialog title';
 $string['dialogtitledesc'] = 'Modal dialog title';
